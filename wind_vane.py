a51db2e97b3fbec8de2784912250de31260790ea
Rafael Dodorico <rafa@satellogic.com>
2018-04-29 15:42:52 -0300

    removes get_value form init

--- wind_vane.py
+++ wind_vane.py
@@ -6,14 +6,14 @@ class WindVaneWithIMU(ComplexSensor):
     """This class represents a IMU-based wind vane,
     which gets inferes wind direction from magnetometer readings."""
 
-    def __init__(self, sensor):
+    def __init__(self, imu_sensor):
         super().__init__()
-        self.sensor = sensor
-        self.sensor_value = sensor.get_value()
+        self.sensor = imu_sensor
 
     def normalize_magnetometer_value(self):
         """Returns the normalized magnetometer vector as a list."""
-        magnetometer_components = sorted(self.sensor_value.values())
+        magnetometer_dict = self.sensor.get_value()
+        magnetometer_components = sorted(magnetometer_dict.values())
         magnetometer_value_norm =  np.sqrt(sum(component**2 for component in magnetometer_components))
         normalized_magnetometer_value = magnetometer_components / magnetometer_value_norm
         return normalized_magnetometer_value
