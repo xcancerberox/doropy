fa3ea62e29f0942059a0b449a6fa9007f95b22f8
Bruno Geninatti <bruno@teknotrol.com>
2018-04-29 23:23:49 -0300

    Add acceleroeter to imu

--- sensors/interfaces.py
+++ sensors/interfaces.py
@@ -2,9 +2,11 @@ import random
 import struct
 import time
 
-from sensors.imu01b import (MAG_CONF_ADD_0, OUT_X_H_M, OUT_X_L_M, OUT_Y_H_M,
-                            OUT_Y_L_M, OUT_Z_H_M, OUT_Z_L_M, TEMP_OUT_H_M,
-                            TEMP_OUT_L_M, MAG_CONF_TEMP_ENABLED)
+from sensors.imu01b import (MAG_CONF_ADD_0, MAG_CONF_TEMP_ENABLED, OUT_X_H_A,
+                            OUT_X_H_M, OUT_X_L_A, OUT_X_L_M, OUT_Y_H_A,
+                            OUT_Y_H_M, OUT_Y_L_A, OUT_Y_L_M, OUT_Z_H_A,
+                            OUT_Z_H_M, OUT_Z_L_A, OUT_Z_L_M, TEMP_OUT_H_M,
+                            TEMP_OUT_L_M)
 
 
 class MockI2C(object):
@@ -12,17 +14,23 @@ class MockI2C(object):
     def __init__(self, address):
         self.address = address
         self.actual_temp = 2000
-        self.actual_x = 2000
-        self.actual_y = 2000
-        self.actual_z = 2000
+        self.actual_mag_x = 2000
+        self.actual_mag_y = 2000
+        self.actual_mag_z = 2000
+        self.actual_acc_x = 2000
+        self.actual_acc_y = 2000
+        self.actual_acc_z = 2000
 
         self.update_values_timeout = time.time()
 
     def update_values(self):
         self.actual_temp = self.get_random_value(self.actual_temp)
-        self.actual_x = self.get_random_value(self.actual_x)
-        self.actual_y = self.get_random_value(self.actual_y)
-        self.actual_z = self.get_random_value(self.actual_z)
+        self.actual_mag_x = self.get_random_value(self.actual_mag_x)
+        self.actual_mag_y = self.get_random_value(self.actual_mag_y)
+        self.actual_mag_z = self.get_random_value(self.actual_mag_z)
+        self.actual_acc_x = self.get_random_value(self.actual_acc_x)
+        self.actual_acc_y = self.get_random_value(self.actual_acc_y)
+        self.actual_acc_z = self.get_random_value(self.actual_acc_z)
 
     def get_random_value(self, previous):
         return int(previous + random.random()*2.5 * random.choice([1, -1]))
@@ -43,17 +51,29 @@ class MockI2C(object):
         elif args[1] == TEMP_OUT_L_M:
             rta = self.get_answer_from_value(self.actual_temp, 1)
         elif args[1] == OUT_X_H_M:
-            rta = self.get_answer_from_value(self.actual_x, 0)
+            rta = self.get_answer_from_value(self.actual_mag_x, 0)
         elif args[1] == OUT_X_L_M:
-            rta = self.get_answer_from_value(self.actual_x, 1)
+            rta = self.get_answer_from_value(self.actual_mag_x, 1)
         elif args[1] == OUT_Y_H_M:
-            rta = self.get_answer_from_value(self.actual_y, 0)
+            rta = self.get_answer_from_value(self.actual_mag_y, 0)
         elif args[1] == OUT_Y_L_M:
-            rta = self.get_answer_from_value(self.actual_y, 1)
+            rta = self.get_answer_from_value(self.actual_mag_y, 1)
         elif args[1] == OUT_Z_H_M:
-            rta = self.get_answer_from_value(self.actual_z, 0)
+            rta = self.get_answer_from_value(self.actual_mag_z, 0)
         elif args[1] == OUT_Z_L_M:
-            rta = self.get_answer_from_value(self.actual_z, 1)
+            rta = self.get_answer_from_value(self.actual_mag_z, 1)
+        elif args[1] == OUT_X_H_A:
+            rta = self.get_answer_from_value(self.actual_acc_x, 0)
+        elif args[1] == OUT_X_L_A:
+            rta = self.get_answer_from_value(self.actual_acc_x, 1)
+        elif args[1] == OUT_Y_H_A:
+            rta = self.get_answer_from_value(self.actual_acc_y, 0)
+        elif args[1] == OUT_Y_L_A:
+            rta = self.get_answer_from_value(self.actual_acc_y, 1)
+        elif args[1] == OUT_Z_H_A:
+            rta = self.get_answer_from_value(self.actual_acc_z, 0)
+        elif args[1] == OUT_Z_L_A:
+            rta = self.get_answer_from_value(self.actual_acc_z, 1)
         elif args[1] == MAG_CONF_ADD_0:
             return MAG_CONF_TEMP_ENABLED
         else:
