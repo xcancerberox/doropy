52e36251511023be41110c7142ec42a23655a9f1
Bruno Geninatti <bruno@teknotrol.com>
2018-05-01 04:05:53 -0300

    anemometer return 0 when no pulses

--- anemometer.py
+++ anemometer.py
@@ -2,7 +2,7 @@ import numpy as np
 from complex_sensor import ComplexSensor
 
 class AnemometerWithTachometer(ComplexSensor):
-    
+
     def __init__(self, sensors, arm_length_meters):
         super().__init__()
         self.sensor = sensors['Tachometer']
@@ -10,7 +10,10 @@ class AnemometerWithTachometer(ComplexSensor):
 
     def get_wind_speed(self):
         rotation_period = self.sensor.get_value()['measurements'][0]['value']
-        rotation_speed = 2*np.pi*(1/rotation_period) 
+        if not rotation_period:
+            return 0
+
+        rotation_speed = 2*np.pi*(1/rotation_period)
         wind_speed_m_s = rotation_speed * self.arm_length_meters
         wind_speed_km_h = 3.6 * wind_speed_m_s
         return wind_speed_km_h
